f0b1f76e2b37178f80a229192cce191e8bb259e8
SEEF <106631218+se-f@users.noreply.github.com>
2024-03-05 19:29:31 +0100

    Update ClientA.java comments

--- src/main/java/org/jee/CouplageFaible/client/ClientA.java
+++ src/main/java/org/jee/CouplageFaible/client/ClientA.java
@@ -4,8 +4,8 @@ import org.jee.CouplageFaible.interfaces.Client;
 import org.jee.CouplageFaible.interfaces.Service;
 
 public class ClientA implements Client {
-    // Le client maintenant interagit avec l'interface Service et non pas l'imlplémentation du service
-    // S'il y a un changement dans l'implémentation du service, le client n'a pas besoin d'être modifié
+    // Le client maintenant interagit avec l'interface Service et non pas l'implémentation de la classe service
+    // S'il y a un changement dans l'implémentation du service, le client n'est pas besoin d'être modifié
     Service service;
 
     public ClientA(Service service) {
